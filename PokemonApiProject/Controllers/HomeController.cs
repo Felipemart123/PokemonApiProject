c8c48a377b3b8a515711c7cf306ca8c1c4c25aec
Felipemart123 <felipemart1876@gmail.com>
2023-02-16 13:33:05 -0500

    repository

---
+++ PokemonApiProject/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PokemonApiProject.Models;
+
+namespace PokemonApiProject.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var pokemon = new PokemonInfo();
+
+        return View(pokemon);
+    }
+
+    public IActionResult Search(PokemonInfo pokemon)
+    {
+        pokemon = APIRepository.GetPokemonName(pokemon.PokemonName);
+        APIRepository.GetPokemonName(pokemon.PokemonName);
+        
+
+        return View(pokemon);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ PokemonApiProject/Models/PokemonInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PokemonApiProject.Models
+{
+	public class PokemonInfo
+	{
+		public string PokemonName { get; set; }
+		public string Ability { get; set; }
+		public string Typing { get; set; }
+		public string HeldItem { get; set; }
+
+	}
+}
+
